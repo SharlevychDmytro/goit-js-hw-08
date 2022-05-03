6abacf8a6221b184e53ff42d6efbde7e078cb592
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-05-03 09:23:05 +0300

    add task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+console.log(throttle);
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const storage = {
+  addItem(key, value) {
+    const result = JSON.stringify(value);
+    localStorage.setItem(key, result);
+  },
+
+  getItem(key) {
+    try {
+      const payload = localStorage.getItem(key);
+      return JSON.parse(payload);
+    } catch (error) {
+      console.error('Error parse');
+    }
+  },
+};
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    storage.addItem('videoplayer-current-time', data.seconds);
+  }, 1000),
+);
+addEventListener('DOMContentLoaded', onPageInit);
+
+function onPageInit() {
+  const seconds = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(seconds);
+}
